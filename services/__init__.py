236120c622e3e2dd75a2710f1b5ea63489b43869
Harish Navnit <harishnavnit@gmail.com>
2021-05-02 13:00:15 +0530

    Update config

--- services/__init__.py
+++ services/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('config.py', silent=False)
+app.config.from_object('services.config')
+app.config.from_pyfile('config.py', silent=True)
 
 
 with app.app_context():
--- services/config.py
+++ services/config.py
@@ -6,3 +6,5 @@ DB_ALIASES = {
     'sa': 'storageaccount',
     'vm': 'virtualmachine'
 }
+
+AZURE_API_VERSION = '2021-01-01'
