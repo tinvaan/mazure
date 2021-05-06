d4758c76eab6596d5a0d440c2f74cc7ed3459fc9
Harish Navnit <harishnavnit@gmail.com>
2021-05-07 02:36:09 +0530

    Fix startup and run scripts

--- mazure/services/__init__.py
+++ mazure/services/__init__.py
@@ -3,11 +3,11 @@ from flask import Flask
 
 
 app = Flask('mazure', instance_relative_config=True)
-app.config.from_object('config')
+app.config.from_object('mazure.config')
 app.config.from_pyfile('config.py', silent=True)
 
 
 with app.app_context():
-    from services.storageaccounts.views import sa
+    from .storageaccounts.views import sa
 
     app.register_blueprint(sa, url_prefix='/subscriptions')
