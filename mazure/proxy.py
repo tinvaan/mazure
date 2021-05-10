245ce38db747fce618c09abae22ddff5e6b76526
Harish Navnit <harishnavnit@gmail.com>
2021-05-10 18:06:33 +0530

    Mock azure API's using the responses library

--- mazure/proxy.py
+++ mazure/proxy.py
@@ -1,5 +1,7 @@
 
 import os
+import re
+import responses
 
 
 class GlobalProxy:
@@ -22,3 +24,37 @@ class GlobalProxy:
             os.environ.pop('HTTP_PROXY')
         if 'HTTPS_PROXY' in os.environ.keys():
             os.environ.pop('HTTPS_PROXY')
+
+
+class AzureInterceptor:
+    HOSTS = [
+        "https://management.azure.com",
+        "https://login.microsoftonline.com"
+    ]
+    METHODS = ["GET", "PUT", "POST", "PATCH", "DELETE"]
+
+    def __init__(self):
+        self.http = responses.mock
+
+    def __enter__(self, *args, **kwargs):
+        self.setup()
+
+    def __exit__(self, *args, **kwargs):
+        self.cleanup()
+
+    def setup(self):
+        self.http.start()
+        for method in self.METHODS:
+            for host in self.HOSTS:
+                self.http.add_callback(
+                    method,
+                    re.compile(host),
+                    callback=self.callback,
+                    content_type='application/json')
+
+    def cleanup(self):
+        self.http.stop()
+        self.http.reset()
+
+    def callback(self, request):
+        raise NotImplementedError('Subclass responsibility')
