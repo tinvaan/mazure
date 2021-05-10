ba70ddbb2b62df0afc5fe29ecd32fafc57d61863
Harish Navnit <harishnavnit@gmail.com>
2021-05-10 18:06:33 +0530

    Mock azure API's using the responses library

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 
 from mazure.services import app
-from mazure.proxy import GlobalProxy
 
-
-GlobalProxy.enable('http://%s:%s' % (
-    app.config.get('MAZURE_SERVER'), app.config.get('MAZURE_PORT')))
 
 app.run(
     debug=True,
