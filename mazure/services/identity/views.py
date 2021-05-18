214284789a33224d8d8b18c2f5c428913effba0e
Harish Navnit <harishnavnit@gmail.com>
2021-05-18 23:54:21 +0530

    Code cleanup

--- mazure/services/identity/views.py
+++ mazure/services/identity/views.py
@@ -1,6 +1,5 @@
 
-from json import dumps
-from flask import Blueprint, jsonify, make_response
+from flask import jsonify, make_response, Blueprint
 
 from .endpoints import read
 
@@ -11,7 +10,6 @@ auth = Blueprint('identity', __name__)
 @auth.route('/common/discovery/instance')
 def discover_instance():
     data = read('discover')
-    return dumps(data.get('body')), data.get('status'), data.get('headers')
     response = make_response(jsonify(data.get('body')), data.get('status'))
     response.headers = data.get('headers')
     return response
@@ -20,7 +18,6 @@ def discover_instance():
 @auth.route('/<subId>/oauth2/v2.0/token', methods=['POST'])
 def oauth_token(subId):
     data = read('oauth')
-    return dumps(data.get('body')), data.get('status'), data.get('headers')
     response = make_response(jsonify(data.get('body')), data.get('status'))
     response.headers = data.get('headers')
     return response
@@ -29,12 +26,6 @@ def oauth_token(subId):
 @auth.route('/<subId>/v2.0/.well-known/openid-configuration')
 def openid_config(subId):
     data = read('openid')
-    return dumps(data.get('body')), data.get('status'), data.get('headers')
-    return (
-        data.get('status'),
-        data.get('headers'),
-        dumps(data.get('body'))
-    )
     response = make_response(jsonify(data.get('body')), data.get('status'))
     response.headers = data.get('headers')
     return response
