f21c1c8218f44d6449667c0734ef9b3ef4f00ce1
Harish Navnit <harishnavnit@gmail.com>
2021-05-21 01:14:06 +0530

    Code and artifact cleanups

--- mazure/services/storageaccounts/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from gzip import compress
-from json import dumps, loads
-from flask import current_app as app
-from flask_mongoengine import MongoEngine
-
-
-store = MongoEngine(app._get_current_object())
