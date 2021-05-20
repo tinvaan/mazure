ac6459ef6cce34516a75ed374750e588b050566c
Harish Navnit <harishnavnit@gmail.com>
2021-05-21 01:14:06 +0530

    Code and artifact cleanups

--- mazure/services/storageaccounts/models.py
+++ mazure/services/storageaccounts/models.py
@@ -2,6 +2,11 @@
 import mongoengine as db
 
 from datetime import datetime
+from flask import current_app as app
+from flask_mongoengine import MongoEngine
+
+
+store = MongoEngine(app._get_current_object())
 
 
 class StorageAccount(db.Document):
