00e882c0ab94b80df4698dc3d894870e4c09b6a2
Harish Navnit <harishnavnit@gmail.com>
2021-05-20 19:27:43 +0530

    BDD test cases for creating and deleting storage accounts

--- tests/features/steps/storageaccounts.py
+++ tests/features/steps/storageaccounts.py
@@ -1,5 +1,5 @@
 
-from behave import given, then
+from behave import given, then, when
 from azure.mgmt.storage.models import (
     StorageAccountCreateParameters,
     StorageAccountCheckNameAvailabilityParameters
@@ -41,12 +41,40 @@ def step_impl(context):
         context.response = client.storage_accounts.list()
 
 
-@then('Return truthy availability')
+@given('Storage account with name "{}" is deleted')
+def step_impl(context, name):
+    client = context.clients.get('sa')
+    with context.proxy:
+        context.response = client.storage_accounts.delete(
+            account_name=name, resource_group_name='testrg')
+    context.name = name
+
+
+@when('New storage account is created with name "{name}"')
+def step_impl(context, name):
+    kws = {
+        'sku': {'name': 'Premium_LRS'},
+        'kind': 'BlockBlobStorage',
+        'location': 'westus'
+    }
+    client = context.clients.get('sa')
+    with context.proxy:
+        try:
+            context.response = client.storage_accounts.begin_create(
+                'testrg', name, StorageAccountCreateParameters(**kws))
+        except Exception:
+            context.error = True
+    context.name = name
+
+
+@then('Storage account name is available')
+@given('Storage account name is available')
 def step_impl(context):
     assert context.response.name_available is True
 
 
-@then('Return falsy availability')
+@then('Storage account name is unavailable')
+@given('Storage account name is unavailable')
 def step_impl(context):
     assert context.response.name_available is False
 
@@ -62,3 +90,32 @@ def step_impl(context):
     with context.proxy:
         for account in context.response:
             assert account
+
+
+@then('Requested name is found in list of storage accounts')
+def step_impl(context):
+    client = context.clients.get('sa')
+    with context.proxy:
+        assert context.name in [
+            account.name for account in client.storage_accounts.list()]
+
+
+@then('Create operation raises an error')
+def step_impl(context):
+    assert context.error is True
+
+
+@then('Deleted account is not found in list of storage accounts')
+def step_impl(context):
+    client = context.clients.get('sa')
+    with context.proxy:
+        assert context.name not in [
+            account.name for account in client.storage_accounts.list()]
+
+
+@then('List of storage accounts is unchanged')
+def step_impl(context):
+    client = context.clients.get('sa')
+    with context.proxy:
+        assert ['first', 'second', 'third'] == [
+            account.name for account in client.storage_accounts.list()]
