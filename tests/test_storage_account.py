e9c8ff9982f65daae02fb141167e44daa5725f1d
Harish Navnit <harishnavnit@gmail.com>
2021-06-16 22:25:45 +0530

    Drop experimental tests

--- tests/test_storage_account.py
+++ tests/test_storage_account.py
@@ -1,6 +1,4 @@
 
-import os
-import vcr
 import uuid
 import secrets
 import unittest
@@ -15,42 +13,6 @@ from azure.mgmt.storage.models import (
 
 from mazure.proxy import AzureProxy
 from mazure.services.storageaccounts.models import StorageAccount
-
-
-@unittest.skip('Used for debugging request/responses')
-class TestStorageAccounts(unittest.TestCase):
-    @vcr.use_cassette('/tmp/azure/auth.yaml')
-    @classmethod
-    def setUpClass(cls):
-        cls.creds = ClientSecretCredential(
-            tenant_id=os.environ.get('AZURE_TENANT_ID'),
-            client_id=os.environ.get('AZURE_CLIENT_ID'),
-            client_secret=os.environ.get('AZURE_SECRET_KEY'))
-        cls.client = StorageManagementClient(
-            cls.creds, os.environ.get('AZURE_SUBSCRIPTION_ID'))
-
-    def setUp(self):
-        self.assertIsNotNone(self.creds)
-        self.assertIsNotNone(self.client)
-
-    @vcr.use_cassette('/tmp/azure/sa.name.yaml')
-    def test_check_storage_account_name(self):
-        valid = StorageAccountCheckNameAvailabilityParameters(name='foobarsa')
-        invalid = StorageAccountCheckNameAvailabilityParameters(name='rampupsa')
-        self.assertTrue(self.client.storage_accounts
-                        .check_name_availability(valid).name_available)
-        self.assertFalse(self.client.storage_accounts
-                         .check_name_availability(invalid).name_available)
-
-    @vcr.use_cassette('/tmp/azure/sa.yaml')
-    def test_list_storage_accounts(self):
-        for account in self.client.storage_accounts.list():
-            self.assertIsNotNone(account)
-
-    @vcr.use_cassette('/tmp/azure/sa.rg.yml')
-    def test_list_storage_accounts_by_rg(self):
-        for account in self.client.storage_accounts.list_by_resource_group('rampup'):
-            self.assertIsNotNone(account)
 
 
 class TestStorageAccountProxy(unittest.TestCase):
