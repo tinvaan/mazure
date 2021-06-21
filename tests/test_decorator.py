45ddb5ffa4118489254b4194c4aed0a388219c81
Harish Navnit <harishnavnit@gmail.com>
2021-06-22 03:18:08 +0530

    Fix decorator tests

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -3,9 +3,11 @@ import uuid
 import secrets
 import unittest
 
+from werkzeug.routing import Map
 from azure.identity import ClientSecretCredential
 from azure.mgmt.storage import StorageManagementClient
-from azure.core.exceptions import ClientAuthenticationError
+from azure.mgmt.compute import ComputeManagementClient
+from azure.core.exceptions import ResourceNotFoundError
 
 from mazure import mazure
 from mazure.services import app
@@ -47,27 +49,33 @@ class TestMazureDecorator(unittest.TestCase):
 
     @mazure('compute')
     def test_invalid_arg_decorator(self):
-        with self.assertRaises(ClientAuthenticationError):
+        with self.assertRaises(NotImplementedError):
             self.assertListEqual(
                 [], [_ for _ in self.client.storage_accounts.list()]
             )
 
     @mazure('storage', allow=True)
     def test_invalid_arg_with_passthru(self):
-        with self.assertRaises(ClientAuthenticationError):
-            self.assertListEqual(
-                [], [_ for _ in self.client.storage_accounts.list()]
-            )
+        self.assertListEqual(
+            [], [_ for _ in self.client.storage_accounts.list()]
+        )
+        with self.assertRaises(ResourceNotFoundError):
+            client = ComputeManagementClient(self.creds, self.subscription)
+            self.assertEqual(
+                [], [_ for _ in client.virtual_machines.list_all()])
 
     @mazure('storage_accounts', allow=True)
-    @unittest.skip('TODO: Implement passthru logic')
     def test_args_kwargs_decorator(self):
-        with self.assertRaises(ClientAuthenticationError):
-            self.assertListEqual(
-                [], [_ for _ in self.client.storage_accounts.list()]
-            )
+        self.assertListEqual(
+            [], [_ for _ in self.client.storage_accounts.list()]
+        )
+        with self.assertRaises(ResourceNotFoundError):
+            client = ComputeManagementClient(self.creds, self.subscription)
+            self.assertEqual(
+                [], [_ for _ in client.virtual_machines.list_all()])
 
     def tearDown(self):
+        app.url_map = Map()
         app.blueprints.clear()
         app.config.update({'ALLOW_AZURE_REQUESTS': False})
 
